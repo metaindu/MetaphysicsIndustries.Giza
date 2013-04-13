b4f709990016af893924cd8767df5ad28eb9d2e6
izrik <izrik@yahoo.com>
2013-04-13 08:56:47 -0500

    Tweaked test script.

--- test.sh
+++ test.sh
@@ -3,3 +3,4 @@
 rm -rf Supergrammar-parsed-new.json
 mono giza/bin/Debug/giza.exe --super Supergrammar.txt > Supergrammar-parsed-new.json
 diff Supergrammar-parsed-ref.json Supergrammar-parsed-new.json
+rm -rf Supergrammar-parsed-new.json
