73282079e8e2d056fa0947220fd82e41b37afe1a
izrik <izrik@yahoo.com>
2013-04-30 12:25:27 -0500

    Added the Grammar class.

---
+++ Grammar.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+
+namespace MetaphysicsIndustries.Giza
+{
+    public class Grammar
+    {
+        public string Name;
+        public List<Definition> Definitions = new List<Definition>();
+    }
+}
+
