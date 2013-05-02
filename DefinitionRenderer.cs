ebbef4ecec595d4fe4c9db48db7690e236d5f28b
izrik <izrik@yahoo.com>
2013-05-02 15:22:03 -0500

    Made rendered classes inherit from Grammar, and removed their Definitions field. Added indents.

--- DefinitionRenderer.cs
+++ DefinitionRenderer.cs
@@ -10,46 +10,43 @@ namespace MetaphysicsIndustries.Giza
         public string RenderDefinitionsAsCSharpClass(string className, IEnumerable<Definition> _defs)
         {
             List<Definition> defs = new List<Definition>(_defs);
-            
+
             Dictionary<Definition, string> defnames = new Dictionary<Definition, string>();
-            
+
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("using System;");
-            sb.AppendLine("using System.Collections.Generic;");
             sb.AppendLine();
             sb.AppendLine("namespace MetaphysicsIndustries.Giza");
             sb.AppendLine("{");
-            sb.AppendFormat("    public class {0}", RenderIdentifier(className));
+            sb.AppendFormat("    public class {0} : Grammar", RenderIdentifier(className));
             sb.AppendLine();
             sb.AppendLine("    {");
-            sb.AppendLine("        List<Definition> Definitions = new List<Definition>();");
-            sb.AppendLine();
-            
+
             foreach (Definition def in defs)
             {
                 string name = string.Format("def_{0}_{1}", def._id, RenderIdentifier(def.Name));
-                
+
                 defnames[def] = name;
-                
+
                 sb.AppendFormat(
                     "        Definition {0} = new Definition({1});",
                     name,
                     RenderString(def.Name));
                 sb.AppendLine();
             }
-            
+
             sb.AppendLine();
-            
+
             Dictionary<Node, string> nodenames = new Dictionary<Node, string>();
-            
+
             foreach (Definition def in defs)
             {
                 foreach (Node node in def.Nodes)
                 {
                     string name = string.Format("node_{0}_{1}_{2}", node._id, RenderIdentifier(def.Name), RenderNodeNameAsIdentifier(node.Tag));
-                    
+
                     nodenames[node] = name;
-                    
+
                     string type = "";
                     if (node is StartNode)
                     {
@@ -67,38 +64,44 @@ namespace MetaphysicsIndustries.Giza
                     {
                         type = "DefRefNode";
                     }
-                    
+
                     sb.AppendFormat("        {0} {1};", type, name);
                     sb.AppendLine();
                 }
             }
-            
+
             sb.AppendLine();
             sb.AppendFormat("        public {0}()", RenderIdentifier(className));
             sb.AppendLine();
             sb.AppendLine("        {");
-            
+
+            string indent = "            ";
             foreach (Definition def in defs)
             {
+                sb.Append(indent);
                 sb.AppendFormat("Definitions.Add({0});", defnames[def]);
                 sb.AppendLine();
             }
-            
+
             sb.AppendLine();
-            
+
             foreach (Definition def in defs)
             {
+                sb.Append(indent);
                 sb.AppendFormat("{0}.IgnoreCase = {1};", defnames[def], (def.IgnoreCase ? "true" : "false"));
                 sb.AppendLine();
+                sb.Append(indent);
                 sb.AppendFormat("{0}.IgnoreWhitespace = {1};", defnames[def], (def.IgnoreWhitespace ? "true" : "false"));
                 sb.AppendLine();
+                sb.Append(indent);
                 sb.AppendFormat("{0}.Contiguous = {1};", defnames[def], (def.Contiguous ? "true" : "false"));
                 sb.AppendLine();
-                
+
                 foreach (Node node in def.Nodes)
                 {
                     string name = nodenames[node];
-                    
+
+                    sb.Append(indent);
                     if (node is StartNode)
                     {
                         sb.AppendFormat("{0} = new StartNode();", name);
@@ -127,10 +130,12 @@ namespace MetaphysicsIndustries.Giza
                 }
                 foreach (Node node in def.Nodes)
                 {
+                sb.Append(indent);
                     sb.AppendFormat("{0}.Nodes.Add({1});", defnames[def], nodenames[node]);
                     sb.AppendLine();
                 }
-                
+
+                sb.Append(indent);
                 sb.AppendFormat("{0}.start = ", defnames[def]);
                 if (def.start == null)
                 {
@@ -141,7 +146,8 @@ namespace MetaphysicsIndustries.Giza
                     sb.Append(nodenames[def.start]);
                 }
                 sb.AppendLine(";");
-                
+
+                sb.Append(indent);
                 sb.AppendFormat("{0}.end = ", defnames[def]);
                 if (def.end == null)
                 {
@@ -152,48 +158,49 @@ namespace MetaphysicsIndustries.Giza
                     sb.Append(nodenames[def.end]);
                 }
                 sb.AppendLine(";");
-                
+
                 foreach (Node node in def.Nodes)
                 {
                     foreach (Node next in node.NextNodes)
                     {
+                        sb.Append(indent);
                         sb.AppendFormat("{0}.NextNodes.Add({1});", nodenames[node], nodenames[next]);
                         sb.AppendLine();
                     }
                 }
                 sb.AppendLine();
             }
-            
+
             sb.AppendLine("        }");
             sb.AppendLine("    }");
             sb.AppendLine("}");
             sb.AppendLine();
-            
+
             return sb.ToString();
         }
-        
+
         static string RenderIdentifier(string s)
         {
             return Regex.Replace(s, @"[^\w\d_]", "_");
         }
-        
+
         static string RenderString(string s)
         {
             string s2 = s;
-            
+
             s2 = s2.Replace("\"", "\\\"");
             s2 = s2.Replace("\\", "\\\\");
             s2 = s2.Replace("\r", "\\r");
             s2 = s2.Replace("\n", "\\n");
             s2 = s2.Replace("\t", "\\t");
-            
+
             return "\"" + s2 + "\"";
         }
-        
+
         static string RenderNodeNameAsIdentifier(string name)
         {
             StringBuilder sb = new StringBuilder();
-            
+
             foreach (char ch in name)
             {
                 if (char.IsLetterOrDigit(ch) || ch == '_')
@@ -207,7 +214,7 @@ namespace MetaphysicsIndustries.Giza
                     sb.Append("_");
                 }
             }
-            
+
             return sb.ToString();
         }
     }
