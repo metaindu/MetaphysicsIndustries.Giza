5a326c3b07e8354cf133173e8a39ed5d74ab783b
izrik <izrik@yahoo.com>
2013-05-15 14:17:19 -0500

    Removed the Span class.

--- Span.cs
+++ Span.cs
@@ -5,44 +5,6 @@ using System.Diagnostics;
 
 namespace MetaphysicsIndustries.Giza
 {
-    [DebuggerDisplay("{Value}")]
-    public class Span
-    {
-        public Span(int start, int length, string input, params Span[] subspans)
-        {
-            if (start < 0) { throw new ArgumentOutOfRangeException("start"); }
-            if (length < 0) { throw new ArgumentOutOfRangeException("length"); }
-
-            Start = start;
-            Length = length;
-            Input = input ?? string.Empty;
-            Subspans = subspans ?? new Span[0];
-        }
-
-        public string Definition;
-
-        public string Tag = string.Empty;
-
-        public string Value
-        {
-            get { return GetText(Input); }
-        }
-
-        public int Start;
-        public int Length;
-        public string Input;
-        public int End
-        {
-            get { return Start + Length - 1; }
-        }
-        public string GetText(string input)
-        {
-            return input.Substring(Start, Length);
-        }
-
-        public Span[] Subspans;
-    }
-
     public class Span2
     {
         public List<Span2> Subspans = new List<Span2>();
