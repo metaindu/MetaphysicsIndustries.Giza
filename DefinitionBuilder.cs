ee450cd3a7e4b4f4f53ee2379a79a63742a786e8
izrik <izrik@yahoo.com>
2013-05-15 12:17:13 -0500

    Added the BuildDefinitions2 method.

--- DefinitionBuilder.cs
+++ DefinitionBuilder.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using MetaphysicsIndustries.Collections;
 using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace MetaphysicsIndustries.Giza
 {
@@ -874,6 +875,513 @@ namespace MetaphysicsIndustries.Giza
 
             short sh = (short)ch;
             return "bits" + sh.ToString("X4");
+        }
+
+        enum DefmodItem
+        {
+            MindWhitespace,
+            IgnoreCase,
+            Contiguous,
+        }
+
+        public Definition[] BuildDefinitions2(Supergrammar grammar, Span2 span)
+        {
+            if (span.Definition != grammar.def_0_grammar) throw new InvalidOperationException();
+//            SpanChecker sc = new SpanChecker();
+//            SpanChecker.Error[] errors = sc.CheckSpan(span, grammar);
+//            if (errors.Length > 0) throw new InvalidOperationException();
+
+            List<Definition> defs = new List<Definition>();
+            Dictionary<Definition, Span2> matchup = new Dictionary<Definition, Span2>();
+            Dictionary<string, Definition> defsByName = new Dictionary<string, Definition>();
+
+            foreach (Span2 sub in span.Subspans)
+            {
+                if (sub.Definition == grammar.def_1_definition)
+                {
+                    Definition def = new Definition();
+                    defs.Add(def);
+                    matchup[def] = sub;
+
+                    foreach (Span2 sub2 in sub.Subspans)
+                    {
+                        if (sub2.Node == grammar.node_5_definition_identifier)
+                        {
+                            def.Name = GetIdentifier(grammar, sub2);
+                            break;
+                        }
+                    }
+                    defsByName[def.Name] = def;
+                }
+                else if (sub.Definition == grammar.def_17_comment)
+                {
+                    //skip it
+                }
+            }
+
+            foreach (Definition def in defs)
+            {
+                Span2 defspan = matchup[def];
+                Set<DefmodItem> defmodItems = new Set<DefmodItem>();
+                foreach (Span2 sub in defspan.Subspans)
+                {
+                    if (sub.Definition == grammar.def_2_defmod)
+                    {
+                        defmodItems.AddRange(GetDefMods(grammar, sub));
+                    }
+                    else if (sub.Definition == grammar.def_13_identifier)
+                    {
+                        // we've already set the name above. skip this node
+                        continue;
+                    }
+                    else if (sub.Definition == null && sub.Node == grammar.node_7_definition__003D_) // '='
+                    {
+                        // skip it
+                        continue;
+                    }
+                    else if (sub.Definition == grammar.def_8_expr)
+                    {
+                        NodeBundle bundle = GetNodesFromExpr(grammar, sub, defsByName);
+
+                        if (bundle.IsSkippable) throw new InvalidOperationException();
+
+                        def.start = new StartNode();
+                        def.start.NextNodes.AddRange(bundle.StartNodes);
+                        def.Nodes.Add(def.start);
+
+                        def.Nodes.AddRange(bundle.Nodes);
+
+                        def.end = new EndNode();
+                        foreach (Node node in bundle.EndNodes)
+                        {
+                            node.NextNodes.Add(def.end);
+                        }
+                        def.Nodes.Add(def.end);
+                    }
+                    else if (sub.Definition == null && sub.Node == grammar.node_9_definition__003B_) // ';'
+                    {
+                        // skip it
+                        continue;
+                    }
+                }
+
+                def.IgnoreWhitespace = true;
+                def.IgnoreCase = false;
+                def.Contiguous = false;
+                foreach (DefmodItem item in defmodItems)
+                {
+                    switch (item)
+                    {
+                    case DefmodItem.MindWhitespace: def.IgnoreWhitespace = false; break;
+                    case DefmodItem.IgnoreCase: def.IgnoreCase = true; break;
+                    case DefmodItem.Contiguous: def.Contiguous = true; break;
+                    }
+                }
+            }
+
+            return defs.ToArray();
+        }
+
+        IEnumerable<DefmodItem> GetDefMods(Supergrammar grammar, Span2 span)
+        {
+            foreach (Span2 sub in span.Subspans)
+            {
+                if (sub.Node == grammar.node_13_defmod_defmod_002D_item ||
+                    sub.Node == grammar.node_16_defmod_defmod_002D_item)
+                {
+                    yield return GetDefModItem(grammar, sub);
+                }
+            }
+        }
+
+        DefmodItem GetDefModItem(Supergrammar grammar, Span2 span)
+        {
+            if (span.Subspans[0].Node == grammar.node_19_defmod_item_id_002D_whitespace)
+            {
+                return DefmodItem.MindWhitespace;
+            }
+            if (span.Subspans[0].Node == grammar.node_20_defmod_item_id_002D_ignore)
+            {
+                if (span.Subspans[1].Node == grammar.node_24_defmod_item_id_002D_case ||
+                    span.Subspans[2].Node == grammar.node_24_defmod_item_id_002D_case)
+                {
+                    return DefmodItem.IgnoreCase;
+                }
+            }
+            if (span.Subspans[0].Node == grammar.node_21_defmod_item_id_002D_contiguous)
+            {
+                return DefmodItem.Contiguous;
+            }
+
+            throw new InvalidOperationException();
+        }
+
+        class NodeBundle
+        {
+            public Set<Node> StartNodes;
+            public Set<Node> EndNodes;
+            public List<Node> Nodes;
+            public bool IsSkippable = false;
+        }
+
+        NodeBundle GetNodesFromExpr(Supergrammar grammar, Span2 exprSpan, Dictionary<string, Definition> defsByName)
+        {
+            NodeBundle first = null;
+            NodeBundle last = null;
+            List<NodeBundle> bundles = new List<NodeBundle>();
+            foreach (Span2 sub in exprSpan.Subspans)
+            {
+                NodeBundle bundle = null;
+                if (sub.Definition == grammar.def_10_subexpr)
+                {
+                    bundle = GetNodesFromSubExpr(grammar, sub, defsByName);
+                }
+                else if (sub.Definition == grammar.def_9_orexpr)
+                {
+                    bundle = GetNodesFromOrExpr(grammar, sub, defsByName);
+                }
+                else if (sub.Definition == grammar.def_17_comment)
+                {
+                    //skip it
+                    continue;
+                }
+
+                if (first == null)
+                {
+                    first = bundle;
+                }
+
+                last = bundle;
+
+                bundles.Add(bundle);
+//                nodes.Add(
+            }
+
+            Set<Node> starts = new Set<Node>();
+            Set<Node> ends = new Set<Node>();
+            List<Node> nodes = new List<Node>();
+
+            foreach (NodeBundle bundle in bundles)
+            {
+                nodes.AddRange(bundle.Nodes);
+            }
+            starts.AddRange(first.StartNodes);
+            ends.AddRange(last.EndNodes);
+
+            // connect the nodes
+            int i;
+            // inter-bundle connections
+            for (i = 1; i < bundles.Count; i++)
+            {
+                foreach (Node prev in bundles[i-1].EndNodes)
+                {
+                    prev.NextNodes.AddRange(bundles[i].StartNodes);
+                }
+            }
+
+            // inter-bundle skips
+            for (i = 2; i < bundles.Count; i++)
+            {
+                if (bundles[i - 1].IsSkippable)
+                {
+                    foreach (Node prev in bundles[i-2].EndNodes)
+                    {
+                        prev.NextNodes.AddRange(bundles[i].StartNodes);
+                    }
+                }
+            }
+
+            // skip from start to inner bundle
+            bool skippable = true;
+            for (i = 1; i < bundles.Count; i++)
+            {
+                if (bundles[i - 1].IsSkippable)
+                {
+                    starts.AddRange(bundles[i].StartNodes);
+                }
+                else
+                {
+                    skippable = false;
+                    break;
+                }
+            }
+            if (skippable)
+            {
+                if (bundles[bundles.Count - 1].IsSkippable)
+                {
+                }
+                else
+                {
+                    skippable = false;
+                }
+            }
+
+            // skip from inner bundle to end
+            for (i = bundles.Count - 1; i > 0; i--)
+            {
+                if (bundles[i].IsSkippable)
+                {
+                    ends.AddRange(bundles[i - 1].EndNodes);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return new NodeBundle{StartNodes = starts,
+                                  EndNodes = ends,
+                                  Nodes = nodes,
+                                  IsSkippable = skippable};
+        }
+
+        NodeBundle GetNodesFromSubExpr(Supergrammar grammar, Span2 span, Dictionary<string, Definition> defsByName)
+        {
+            bool skippable = false;
+            bool loop = false;
+            string tag = null;
+            List<Node> nodes = null;
+
+            foreach (Span2 sub in span.Subspans)
+            {
+                if (sub.Node == grammar.node_77_subexpr_identifier)
+                {
+                    Node node = GetNodeFromIdentifier(grammar, sub, defsByName);
+                    nodes = new List<Node>{node};
+                }
+                else if (sub.Definition == grammar.def_14_literal)
+                {
+                    nodes = GetNodesFromLiteral(grammar, sub);
+                }
+                else if (sub.Definition == grammar.def_15_charclass)
+                {
+                    Node node = GetNodeFromCharClass(grammar, sub);
+                    nodes = new List<Node>{node};
+                }
+                else if (sub.Definition == grammar.def_11_modifier)
+                {
+                    char mod = GetModifier(grammar, sub);
+                    switch (mod)
+                    {
+                    case '?': skippable = true; loop = false; break;
+                    case '+': skippable = false; loop = true; break;
+                    case '*': skippable = true; loop = true; break;
+                    }
+                }
+                else if (sub.Node == grammar.node_81_subexpr__003A_) // ':'
+                {
+                    //skip it
+                    continue;
+                }
+                else if (sub.Node == grammar.node_83_subexpr_tag)
+                {
+                    tag = GetIdentifier(grammar, sub);
+                }
+            }
+
+            if (tag != null)
+            {
+                foreach (Node node in nodes)
+                {
+                    node.SetTag(tag);
+                }
+            }
+
+            int i;
+            for (i = 1; i < nodes.Count; i++)
+            {
+                nodes[i-1].NextNodes.Add(nodes[i]);
+            }
+
+            Node start = nodes[0];
+            Node end = nodes[nodes.Count - 1];
+            if (loop)
+            {
+                end.NextNodes.Add(start);
+            }
+
+            return
+                new NodeBundle {
+                    StartNodes = new Set<Node>{ start },
+                    EndNodes = new Set<Node>{ end },
+                    Nodes = nodes,
+                    IsSkippable = skippable};
+        }
+
+        Node GetNodeFromIdentifier(Supergrammar grammar, Span2 span, Dictionary<string, Definition> defsByName)
+        {
+            string ident = GetIdentifier(grammar, span);
+            return new DefRefNode(defsByName[ident], ident);
+        }
+
+        List<Node> GetNodesFromLiteral(Supergrammar grammar, Span2 span)
+        {
+            List<Node> nodes = new List<Node>();
+            List<char> tagNodes = new List<char>();
+
+            int i;
+            for (i = 1; i < span.Subspans.Count - 1; i++)
+            {
+                Span2 sub = span.Subspans[i];
+                if (sub.Node == grammar.node_95_literal__0027_) continue;
+                if (sub.Node == grammar.node_99_literal__0027_) continue;
+
+                char ch = ' ';
+                if (sub.Definition == grammar.def_16_unicodechar)
+                {
+                    ch = GetUnicodeChar(grammar, sub);
+                }
+                else if (sub.Node == grammar.node_96_literal__005E__005C__005C__0027_) // [^\\']
+                {
+                    ch = sub.Value[0];
+                }
+                else if (sub.Node == grammar.node_97_literal__005C_) // '\\'
+                {
+                    Span2 sub2 = span.Subspans[i+1];
+
+                    if (sub2.Value == "r") ch = '\r';
+                    else if (sub2.Value == "n") ch = '\n';
+                    else if (sub2.Value == "t") ch = '\t';
+                    else if (sub2.Value == "\\") ch = '\\';
+                    else if (sub2.Value == "'") ch = '\'';
+
+                    i++;
+                }
+                nodes.Add(new CharNode(ch));
+                tagNodes.Add(ch);
+            }
+
+            string tag = new string(tagNodes.ToArray());
+            foreach (Node node in nodes)
+            {
+                node.SetTag(tag);
+            }
+
+            return nodes;
+        }
+
+        char GetUnicodeChar(Supergrammar grammar, Span2 span)
+        {
+            string s =
+                span.Subspans[2].Value +
+                span.Subspans[3].Value +
+                span.Subspans[4].Value +
+                span.Subspans[5].Value;
+
+            int i;
+            if (!int.TryParse(s, NumberStyles.HexNumber, null, out i)) throw new InvalidOperationException();
+
+            return (char)i;
+        }
+
+        Node GetNodeFromCharClass(Supergrammar grammar, Span2 span)
+        {
+            int i;
+            List<char> items = new List<char>();
+            for (i = 1; i < span.Subspans.Count - 1; i++)
+            {
+                Span2 sub = span.Subspans[i];
+
+                if (sub.Node == grammar.node_104_charclass__005E__005C__005C__005C__005B__005C__005D_) // [^\\\[\]]
+                {
+                    items.Add(sub.Value[0]);
+                }
+                else if (sub.Node == grammar.node_105_charclass__005C_) // '\\'
+                {
+                    items.Add(sub.Value[0]);
+                }
+                else if (sub.Node == grammar.node_108_charclass_wldsrnt_005C__005C__005C__005B__005C__005D_) // [wldsrnt\\\[\]]
+                {
+                    items.Add(sub.Value[0]);
+                }
+                else if (sub.Node == grammar.node_106_charclass_unicodechar)
+                {
+                    items.Add(GetUnicodeChar(grammar, sub));
+                }
+            }
+
+            string charClassText = new string(items.ToArray());
+
+            return new CharNode(CharClass.FromUndelimitedCharClassText(charClassText));
+        }
+
+        char GetModifier(Supergrammar grammar, Span2 span)
+        {
+            return span.Subspans[0].Value[0];
+        }
+
+        string GetIdentifier(Supergrammar grammar, Span2 span)
+        {
+            List<char> chs = new List<char>();
+            foreach (Span2 sub in span.Subspans)
+            {
+                chs.Add(sub.Value[0]);
+            }
+            return new string(chs.ToArray());
+        }
+
+        NodeBundle GetNodesFromOrExpr(Supergrammar grammar, Span2 span, Dictionary<string, Definition> defsByName)
+        {
+            List<NodeBundle> bundles = new List<NodeBundle>();
+            bool skippable = false;
+            bool loopable = false;
+
+            foreach (Span2 sub in span.Subspans)
+            {
+                if (sub.Node == grammar.node_69_orexpr__0028_ || // '('
+                    sub.Node == grammar.node_71_orexpr__007C_ || // '|'
+                    sub.Node == grammar.node_72_orexpr__0029_)   // ')'
+                {
+                    // skip it
+                    continue;
+                }
+
+                if (sub.Node == grammar.node_70_orexpr_expr ||
+                    sub.Node == grammar.node_73_orexpr_expr)
+                {
+                    bundles.Add(GetNodesFromExpr(grammar, sub, defsByName));
+                    continue;
+                }
+
+                if (sub.Node == grammar.node_74_orexpr_modifier)
+                {
+                    switch (GetModifier(grammar, sub))
+                    {
+                    case '*': loopable = true; skippable = true; break;
+                    case '?': skippable = true; break;
+                    case '+': loopable = true; break;
+                    }
+                }
+            }
+
+            Set<Node> starts = new Set<Node>();
+            Set<Node> ends = new Set<Node>();
+            List<Node> nodes = new List<Node>();
+            foreach (NodeBundle bundle in bundles)
+            {
+                nodes.AddRange(bundle.Nodes);
+                starts.AddRange(bundle.StartNodes);
+                ends.AddRange(bundle.EndNodes);
+                if (bundle.IsSkippable)
+                {
+                    skippable = true;
+                }
+            }
+
+            if (loopable)
+            {
+                foreach (Node end in ends)
+                {
+                    end.NextNodes.AddRange(starts);
+                }
+            }
+
+            return new NodeBundle{
+                StartNodes=starts,
+                EndNodes=ends,
+                Nodes=nodes,
+                IsSkippable=skippable
+            };
         }
     }
 }
