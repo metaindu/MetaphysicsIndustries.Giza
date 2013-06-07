6fbeeadb83678708f9dbdc9fa41aaac95c1dfaca
izrik <izrik@yahoo.com>
2013-06-06 22:00:47 -0500

    Added the Token struct and an empty Tokenizer class.

---
+++ Token.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MetaphysicsIndustries.Giza
+{
+    public struct Token
+    {
+        public Definition Definition;
+        public string Value;
+        public int StartIndex;
+    }
+}
+
