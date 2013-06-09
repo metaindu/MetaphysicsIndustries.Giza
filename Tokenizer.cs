e3fc8bd5d089ea6d1baf9a9e159b1ae95fa3ec72
izrik <izrik@yahoo.com>
2013-06-08 20:58:17 -0500

    Added to Tokenizer.

--- Tokenizer.cs
+++ Tokenizer.cs
@@ -1,11 +1,35 @@
 using System;
+using System.Collections.Generic;
 
 namespace MetaphysicsIndustries.Giza
 {
     public class Tokenizer
     {
-        public Tokenizer()
+        public Tokenizer(Grammar grammar)
         {
+            _tokenDef = new Definition("$token");
+
+            foreach (Definition def in grammar.Definitions)
+            {
+                if (def.Directives.Contains(DefinitionDirective.Token))
+                {
+                    DefRefNode node = new DefRefNode(def);
+                    _tokenDef.Nodes.Add(node);
+                    _tokenDef.StartNodes.Add(node);
+                    _tokenDef.EndNodes.Add(node);
+                }
+            }
+        }
+
+        Spanner _spanner = new Spanner();
+        Definition _tokenDef;
+
+        public Token[] GetTokensAtLocation(Grammar grammar, string input, int index)
+        {
+            string error;
+            Spanner.NodeMatch[] matchTreeLeaves = _spanner.Match(_tokenDef, input, out error);
+
+            throw new NotImplementedException();
         }
     }
 }
