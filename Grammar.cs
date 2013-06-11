ab96691d8e4a92fb35c987fd0fa08f4dfe69c8c7
izrik <izrik@yahoo.com>
2013-06-10 20:08:50 -0500

    Added the Clone method.

--- Grammar.cs
+++ Grammar.cs
@@ -15,6 +15,59 @@ namespace MetaphysicsIndustries.Giza
         {
             get { return _definitions; }
         }
+
+        public Grammar Clone()
+        {
+            Dictionary<Definition, Definition> defsMatchup = new Dictionary<Definition, Definition>();
+            List<Definition> defs = new List<Definition>();
+            foreach (Definition def in this.Definitions)
+            {
+                Definition def2 = new Definition(def.Name);
+                defs.Add(def2);
+                defsMatchup[def] = def2;
+            }
+            Dictionary<Node, Node> nodeMatchup = new Dictionary<Node, Node>();
+            foreach (Definition def in this.Definitions)
+            {
+                Definition def2 = defsMatchup[def];
+                foreach (Node node in def.Nodes)
+                {
+                    Node node2;
+                    if (node is CharNode)
+                    {
+                        node2 = new CharNode((node as CharNode).CharClass, node.Tag);
+                    }
+                    else
+                    {
+                        node2 = new DefRefNode(defsMatchup[(node as DefRefNode).DefRef], node.Tag);
+                    }
+                    nodeMatchup[node] = node2;
+                    def2.Nodes.Add(node2);
+                }
+                foreach (Node node in def.Nodes)
+                {
+                    Node node2 = nodeMatchup[node];
+                    foreach (Node next in node.NextNodes)
+                    {
+                        node2.NextNodes.Add(nodeMatchup[next]);
+                    }
+                }
+                foreach (Node start in def.StartNodes)
+                {
+                    def2.StartNodes.Add(nodeMatchup[start]);
+                }
+                foreach (Node end in def.EndNodes)
+                {
+                    def2.EndNodes.Add(nodeMatchup[end]);
+                }
+                def2.Directives.AddRange(def.Directives);
+            }
+
+            Grammar grammar = new Grammar();
+            grammar.Definitions.AddRange(defs);
+
+            return grammar;
+        }
     }
 }
 
