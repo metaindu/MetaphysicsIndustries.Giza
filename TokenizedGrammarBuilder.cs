986327f82d5701795b41752e19d0802eceed0601
izrik <izrik@yahoo.com>
2013-06-19 09:43:57 -0500

    Added empty TokenizedGrammarBuilder class.

---
+++ TokenizedGrammarBuilder.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MetaphysicsIndustries.Giza
+{
+    public class TokenizedGrammarBuilder
+    {
+        public Grammar BuildTokenizedGrammar(DefinitionInfo[] defs)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
