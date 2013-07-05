ab155f150ff07089cbf843f52611065dff6a4958
izrik <izrik@yahoo.com>
2013-07-05 08:02:52 -0500

    Added the EnumerateLiterals and EnumerateCharClasses methods.

--- Expression.cs
+++ Expression.cs
@@ -27,6 +27,48 @@ namespace MetaphysicsIndustries.Giza
                 }
             }
         }
+
+        public IEnumerable<LiteralSubExpression> EnumerateLiterals()
+        {
+            foreach (var item in this.Items)
+            {
+                if (item is LiteralSubExpression)
+                {
+                    yield return (item as LiteralSubExpression);
+                }
+                else if (item is OrExpression)
+                {
+                    foreach (var expr in (item as OrExpression).Expressions)
+                    {
+                        foreach (var literal in expr.EnumerateLiterals())
+                        {
+                            yield return literal;
+                        }
+                    }
+                }
+            }
+        }
+
+        public IEnumerable<CharClassSubExpression> EnumerateCharClasses()
+        {
+            foreach (var item in this.Items)
+            {
+                if (item is CharClassSubExpression)
+                {
+                    yield return (item as CharClassSubExpression);
+                }
+                else if (item is OrExpression)
+                {
+                    foreach (var expr in (item as OrExpression).Expressions)
+                    {
+                        foreach (var cc in expr.EnumerateCharClasses())
+                        {
+                            yield return cc;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
 
