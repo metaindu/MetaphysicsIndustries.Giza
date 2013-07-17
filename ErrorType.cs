90ff26e2f7d458274a04566c3dddbd11abb093c9
izrik <izrik@yahoo.com>
2013-07-17 10:28:13 -0500

    Added the ErrorType struct, to form the basis of a unified error reporting mechanism.

---
+++ ErrorType.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MetaphysicsIndustries.Giza
+{
+    public struct ErrorType
+    {
+        public string Name;
+        public bool IsWarning;
+        public string Description;
+    }
+}
+
