cc5b15ac655bfa7e7914dbe843bff2e13111035a
izrik <izrik@yahoo.com>
2013-07-19 09:53:10 -0500

    Added some machinery to ErrorType for testing equality and overriding operators.

--- ErrorType.cs
+++ ErrorType.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 
 namespace MetaphysicsIndustries.Giza
 {
-    public struct ErrorType
+    public struct ErrorType : IEquatable<ErrorType>
     {
         public string Name;
         public bool IsWarning;
         public string Description;
+
+        public static bool operator==(ErrorType a, ErrorType b)
+        {
+            return a.Equals(b);
+        }
+        public static bool operator!=(ErrorType a, ErrorType b)
+        {
+            return !a.Equals(b);
+        }
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is ErrorType)) return false;
+            return Equals((ErrorType)obj);
+        }
+        public bool Equals(ErrorType other)
+        {
+            return
+                Name.Equals(other.Name) &&
+                IsWarning.Equals(other.IsWarning) &&
+                Description.Equals(other.Description);
+        }
+        public override int GetHashCode()
+        {
+            return Name.GetHashCode() ^ IsWarning.GetHashCode() ^ Description.GetHashCode();
+        }
     }
 
     public static class ErrorTypeHelpers
