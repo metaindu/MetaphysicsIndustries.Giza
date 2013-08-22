f74475a26b3eec3cbc1b3177f9ef63ce93a1a35f
izrik <izrik@yahoo.com>
2013-08-22 09:05:22 -0500

    Re-work and expand GetTokensAtLocation so that it works with comments properly.

--- Tokenizer.cs
+++ Tokenizer.cs
@@ -29,58 +29,168 @@ namespace MetaphysicsIndustries.Giza
         Spanner _spanner = new Spanner();
         Definition _tokenDef;
 
+        struct TokenizationByIndex
+        {
+            public TokenizationByIndex(int index,
+                                       ICollection<Error> errors,
+                                       bool endOfInput,
+                                       ICollection<NodeMatch> matchTreeLeaves)
+            {
+                Index = index;
+                Errors = errors;
+                EndOfInput = endOfInput;
+                MatchTreeLeaves = matchTreeLeaves;
+            }
+
+            public int Index;
+            public ICollection<Error> Errors;
+            public bool EndOfInput;
+            public ICollection<NodeMatch> MatchTreeLeaves;
+        }
+
         public Token[] GetTokensAtLocation(string input, int index, List<Error> errors, out bool endOfInput)
         {
-            Set<NodeMatch> matchTreeLeaves = new Set<NodeMatch>();
-
-            Queue<int> startIndexes = new Queue<int>();
+            var tokenizations = new Queue<TokenizationByIndex>();
+            var startIndexes = new Queue<int>();
             startIndexes.Enqueue(index);
-            endOfInput = false;
+
             while (startIndexes.Count > 0)
             {
                 int startIndex = startIndexes.Dequeue();
-                var leaves = _spanner.Match(_tokenDef, input, errors,
-                                            out endOfInput,
+
+                bool endOfInput2;
+                var errors2 = new List<Error>();
+                var tokenLeaves = new Set<NodeMatch>();
+
+                var leaves = _spanner.Match(_tokenDef, input, errors2,
+                                            out endOfInput2,
                                             mustUseAllInput:false,
                                             startIndex:startIndex);
-                if (errors.Count > 0)
+
+                if (!errors2.ContainsNonWarnings())
                 {
-                    return null;
+                    foreach (var leaf in leaves)
+                    {
+                        var tokenEnd = leaf.Previous;
+                        if (tokenEnd.DefRef.Directives.Contains(DefinitionDirective.Comment))
+                        {
+                            var tokenStart = tokenEnd.StartDef;
+                            var stindex = tokenStart.Index;
+                            var length = leaf.Index - tokenStart.Index + 1;
+
+                            //skip the comment and start again
+                            startIndexes.Enqueue(stindex + length);
+                        }
+                        else
+                        {
+                            tokenLeaves.Add(leaf);
+                            endOfInput = false;
+                        }
+                    }
                 }
 
-                foreach (var leaf in leaves)
+                if (tokenLeaves.Count > 0 ||
+                    endOfInput2 ||
+                    errors.Count > 0)
                 {
-                    var tokenEnd = leaf.Previous;
-                    if (tokenEnd.DefRef.Directives.Contains(DefinitionDirective.Comment))
-                    {
-                        var tokenStart = tokenEnd.StartDef;
-                        var stindex = tokenStart.Index;
-                        var length = leaf.Index - tokenStart.Index + 1;
+                    var tokenization = new TokenizationByIndex(
+                        startIndex, errors2, endOfInput2, tokenLeaves);
 
-                        //skip the comment and start again
-                        startIndexes.Enqueue(stindex + length);
-                    }
-                    else
-                    {
-                        matchTreeLeaves.Add(leaf);
-                    }
+                    tokenizations.Enqueue(tokenization);
                 }
             }
 
-            List<Token> tokens = new List<Token>();
-            foreach (NodeMatch leaf in matchTreeLeaves)
+            // if any has leaves, then all available leaves will be returned,
+            // no errors will be returned, and endOfInput will be false
+            //
+            // otherwise, if any has EndOfInput=true, then no leaves will be
+            // returned, no errors will be returned, and endOfInput will be
+            // true
+            //
+            // otherwise, no leaves will be returned, the errors of the first
+            // tokenization (by StartIndex) will be returned, and endOfInput
+            // will be false
+
+            var hasLeaves = new List<TokenizationByIndex>();
+            var hasEnd = new List<TokenizationByIndex>();
+            var hasErrors = new List<TokenizationByIndex>();
+
+            foreach (var tok in tokenizations)
             {
-                NodeMatch tokenEnd = leaf.Previous;
-                NodeMatch tokenStart = tokenEnd.StartDef;
-
-                tokens.Add(new Token{
-                    Definition = tokenEnd.Previous.Node.ParentDefinition,
-                    StartIndex = tokenStart.Index,
-                    Length = leaf.Index - tokenStart.Index + 1
-                });
+                if (tok.MatchTreeLeaves.Count > 0 &&
+                    !tok.Errors.ContainsNonWarnings() &&
+                    !tok.EndOfInput)
+                {
+                    hasLeaves.Add(tok);
+                }
+                else if (tok.EndOfInput &&
+                         !tok.Errors.ContainsNonWarnings())
+                {
+                    hasEnd.Add(tok);
+                }
+                else if (tok.Errors.ContainsNonWarnings())
+                {
+                    hasErrors.Add(tok);
+                }
+                else
+                {
+                    // no tokens, no end, no errors?
+                    throw new InvalidOperationException();
+                }
             }
 
-            return tokens.ToArray();
+            if (hasLeaves.Count > 0)
+            {
+                var matchTreeLeaves = new Set<NodeMatch>();
+
+                foreach (var tok in hasLeaves)
+                {
+                    matchTreeLeaves.AddRange(tok.MatchTreeLeaves);
+                }
+
+                List<Token> tokens = new List<Token>();
+                foreach (NodeMatch leaf in matchTreeLeaves)
+                {
+                    NodeMatch tokenEnd = leaf.Previous;
+                    NodeMatch tokenStart = tokenEnd.StartDef;
+
+                    tokens.Add(new Token{
+                        Definition = tokenEnd.Previous.Node.ParentDefinition,
+                        StartIndex = tokenStart.Index,
+                        Length = leaf.Index - tokenStart.Index + 1
+                    });
+                }
+
+                endOfInput = false;
+                return tokens.ToArray();
+            }
+            else if (hasEnd.Count > 0)
+            {
+                endOfInput = true;
+                return new Token[0];
+            }
+            else
+            {
+                var minIndex = hasErrors[0].Index;
+                var mintok = hasErrors[0];
+
+                foreach (var tok in tokenizations)
+                {
+                    if (tok.Index < minIndex)
+                    {
+                        minIndex = tok.Index;
+                        mintok = tok;
+                    }
+                }
+
+                errors.AddRange(mintok.Errors);
+
+                endOfInput = false;
+                return new Token[0];
+            }
+
+
+
         }
     }
 }
