876d68a80bf35af46e6b9a8b26e79571861c2664
izrik <izrik@yahoo.com>
2013-09-04 09:14:01 -0500

    Add the Value field and an easy way to set it.

--- Token.cs
+++ Token.cs
@@ -7,6 +7,12 @@ namespace MetaphysicsIndustries.Giza
         public Definition Definition;
         public int StartIndex;
         public int Length;
+
+        public string Value;
+        public void SetValueFromInput(string input)
+        {
+            Value = input.Substring(StartIndex, Length);
+        }
     }
 }
 
