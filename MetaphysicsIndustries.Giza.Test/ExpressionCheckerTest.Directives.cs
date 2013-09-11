1a29d4558c8b635c62606ff7e26a028b9f916bb7
izrik <izrik@yahoo.com>
2013-09-11 10:56:04 -0500

    Add tests for atomic and mind-whitespace directives in parsed grammars.

--- MetaphysicsIndustries.Giza.Test/ExpressionCheckerTest.Directives.cs
+++ MetaphysicsIndustries.Giza.Test/ExpressionCheckerTest.Directives.cs
@@ -139,6 +139,188 @@ namespace MetaphysicsIndustries.Giza.Test
             var err = (errors[0] as EcError);
             Assert.AreSame(defs[0], err.DefinitionInfo);
         }
+
+        [Test]
+        public void TestAtomicInNonTokenDefinition()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.Atomic);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.AtomicInNonTokenDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
+
+        [Test]
+        public void TestMindWhitespaceInNonTokenDefinition()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.MindWhitespace);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.MindWhitespaceInNonTokenDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
+
+        [Test]
+        public void TestAtomicInTokenizedDefinition1()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.Atomic);
+            defs[0].Directives.Add(DefinitionDirective.Token);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.AtomicInTokenizedDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
+
+        [Test]
+        public void TestAtomicInTokenizedDefinition2()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.Atomic);
+            defs[0].Directives.Add(DefinitionDirective.Subtoken);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.AtomicInTokenizedDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
+
+        [Test]
+        public void TestAtomicInTokenizedDefinition3()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.Atomic);
+            defs[0].Directives.Add(DefinitionDirective.Comment);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.AtomicInTokenizedDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
+
+        [Test]
+        public void TestMindWhitespaceInTokenizedDefinition1()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.MindWhitespace);
+            defs[0].Directives.Add(DefinitionDirective.Token);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.MindWhitespaceInTokenizedDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
+
+        [Test]
+        public void TestMindWhitespaceInTokenizedDefinition2()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.MindWhitespace);
+            defs[0].Directives.Add(DefinitionDirective.Subtoken);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.MindWhitespaceInTokenizedDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
+
+        [Test]
+        public void TestMindWhitespaceInTokenizedDefinition3()
+        {
+            DefinitionExpression[] defs = {
+                new DefinitionExpression {
+                    Name = "A",
+                },
+            };
+            defs[0].Directives.Add(DefinitionDirective.MindWhitespace);
+            defs[0].Directives.Add(DefinitionDirective.Comment);
+            defs[0].Items.Add(new LiteralSubExpression { Value = "literal" });
+
+            ExpressionChecker ec = new ExpressionChecker();
+            List<Error> errors = ec.CheckDefinitionInfosForParsing(defs);
+
+            Assert.IsNotNull(errors);
+            Assert.AreEqual(1, errors.Count);
+            Assert.AreEqual(EcError.MindWhitespaceInTokenizedDefinition, errors[0].ErrorType);
+            Assert.IsInstanceOf<EcError>(errors[0]);
+            var err = (errors[0] as EcError);
+            Assert.AreSame(defs[0], err.DefinitionInfo);
+        }
     }
 }
 
