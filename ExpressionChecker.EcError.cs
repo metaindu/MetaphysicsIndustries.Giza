d638c99dffcda0b8007c9b8e7d1ae66076c0191e
izrik <izrik@yahoo.com>
2013-09-18 09:30:53 -0500

    Expound some more error descriptions.

--- ExpressionChecker.EcError.cs
+++ ExpressionChecker.EcError.cs
@@ -54,44 +54,45 @@ namespace MetaphysicsIndustries.Giza
             }
 
             // all grammars
-            public static readonly ErrorType ReusedDefintion =                          new ErrorType(name:"ReusedDefintion");
-            public static readonly ErrorType NullDefinition =                           new ErrorType(name:"NullDefinition");
-            public static readonly ErrorType ReusedExpressionOrItem =                   new ErrorType(name:"ReusedExpressionOrItem");
-            public static readonly ErrorType NullOrEmptyDefinitionName =                new ErrorType(name:"NullOrEmptyDefinitionName");
-            public static readonly ErrorType EmptyExpressionItems =                     new ErrorType(name:"EmptyExpressionItems");
-            public static readonly ErrorType NullExpressionItem =                       new ErrorType(name:"NullExpressionItem");
-            public static readonly ErrorType EmptyOrexprExpressionList =                new ErrorType(name:"EmptyOrexprExpressionList");
-            public static readonly ErrorType NullOrexprExpression =                     new ErrorType(name:"NullOrexprExpression");
-            public static readonly ErrorType NullSubexprTag =                           new ErrorType(name:"NullSubexprTag");
-            public static readonly ErrorType NullOrEmptyDefrefName =                    new ErrorType(name:"NullOrEmptyDefrefName");
-            public static readonly ErrorType NullOrEmptyLiteralValue =                  new ErrorType(name:"NullOrEmptyLiteralValue");
-            public static readonly ErrorType NullOrEmptyCharClass =                     new ErrorType(name:"NullOrEmptyCharClass");
-            public static readonly ErrorType DuplicateDefinitionName =                  new ErrorType(name:"DuplicateDefinitionName");
-            public static readonly ErrorType AllItemsSkippable =                        new ErrorType(name:"AllItemsSkippable");
-            public static readonly ErrorType SkippableOrexprExpressions =               new ErrorType(name:"SkippableOrexprExpressions ");
-            public static readonly ErrorType DefRefNameNotFound =                       new ErrorType(name:"DefRefNameNotFound ");
+            //  technical errors - can only occur when manually creating the
+            //      object tree. SupergrammarSpanner does not generate expression
+            //      trees with these errors.
+            public static readonly ErrorType ReusedDefintion                            = new ErrorType(name:"ReusedDefintion",                                         descriptionFormat:"The DefinitionExpression object for '{DefinitionInfoName}' was re-used at index {Index}. ");
+            public static readonly ErrorType NullDefinition                             = new ErrorType(name:"NullDefinition",                                          descriptionFormat:"A null was found in place of a DefinitionExpression object, at index {Index}. ");
+            public static readonly ErrorType ReusedExpressionOrItem                     = new ErrorType(name:"ReusedExpressionOrItem",                                  descriptionFormat:"{Name}");
+            public static readonly ErrorType NullOrEmptyDefinitionName                  = new ErrorType(name:"NullOrEmptyDefinitionName",                               descriptionFormat:"The definition's name was null or empty, at index {Index}. ");
+            public static readonly ErrorType EmptyExpressionItems                       = new ErrorType(name:"EmptyExpressionItems",                                    descriptionFormat:"An Expression object's Items collection is empty in definition {DefinitionInfoName}. ");
+            public static readonly ErrorType NullExpressionItem                         = new ErrorType(name:"NullExpressionItem",                                      descriptionFormat:"A null reference was found in place of an ExpressionItem object, at index {Index}, in definition {DefinitionInfoName}. ");
+            public static readonly ErrorType EmptyOrexprExpressionList                  = new ErrorType(name:"EmptyOrexprExpressionList",                               descriptionFormat:"An OrExpression object's Expressions collection is empty, in definition {DefinitionInfoName}. ");
+            public static readonly ErrorType NullOrexprExpression                       = new ErrorType(name:"NullOrexprExpression",                                    descriptionFormat:"A null reference was found in place of an Expression object within an OrExpression object's Expressions collection, at index {Index}, in definition {DefinitionInfoName}. ");
+            public static readonly ErrorType NullSubexprTag                             = new ErrorType(name:"NullSubexprTag",                                          descriptionFormat:"A SubExpression object's Tag field is null, in definition {DefinitionInfoName}. ");
+            public static readonly ErrorType NullOrEmptyDefrefName                      = new ErrorType(name:"NullOrEmptyDefrefName",                                   descriptionFormat:"A DefRefSubExpression object's DefinitionName field is null or empty, in definition {DefinitionInfoName}. ");
+            public static readonly ErrorType NullOrEmptyLiteralValue                    = new ErrorType(name:"NullOrEmptyLiteralValue",                                 descriptionFormat:"A LiteralSubExpression object's Value field is null or empty, in definition {DefinitionInfoName}. ");
+            public static readonly ErrorType NullOrEmptyCharClass                       = new ErrorType(name:"NullOrEmptyCharClass",                                    descriptionFormat:"A CharClassSubExpression object's CharClass field is null or empty, in definition {DefinitionInfoName}. ");
+
+            //  grammar errors - SupergrammarSpanner can emit expression trees
+            //      from grammar files with these errors.
+            public static readonly ErrorType DuplicateDefinitionName                    = new ErrorType(name:"DuplicateDefinitionName",                                 descriptionFormat:"The definition '{DefinitionInfoName}' has already been defined.");
+            public static readonly ErrorType AllItemsSkippable                          = new ErrorType(name:"AllItemsSkippable",                                       descriptionFormat:"{Name}");
+            public static readonly ErrorType SkippableOrexprExpressions                 = new ErrorType(name:"SkippableOrexprExpressions",                              descriptionFormat:"One of the expressions within an OrExpression in ");
+            public static readonly ErrorType DefRefNameNotFound                         = new ErrorType(name:"DefRefNameNotFound",                                      descriptionFormat:"Definition '{DefinitionInfoName}' references a definition '{ReferencedDefinitionName}' which is not defined.");
 
             // spanned grammars
-            public static readonly ErrorType TokenizedDirectiveInNonTokenizedGrammar =  new ErrorType(name:"TokenizedDirectiveInNonTokenizedGrammar",   isWarning:true );
+            public static readonly ErrorType TokenizedDirectiveInNonTokenizedGrammar    = new ErrorType(name:"TokenizedDirectiveInNonTokenizedGrammar", isWarning:true, descriptionFormat:"One of directives 'token', 'subtoken', or 'comment' was applied to definition '{DefinitionInfoName}', which is in a spanned grammar. The directive will be ignored.");
 
             // parsed grammars
-            public static readonly ErrorType MixedTokenizedDirectives =                 new ErrorType(name:"MixedTokenizedDirectives");
-            public static readonly ErrorType NonTokenReferencesSubtoken =               new ErrorType(name:"NonTokenReferencesSubtoken");
-            public static readonly ErrorType NonTokenReferencesComment =                new ErrorType(name:"NonTokenReferencesComment");
-            public static readonly ErrorType TokenizedReferencesNonToken =              new ErrorType(name:"SubtokenReferencesNonToken");
-            public static readonly ErrorType TokenizedReferencesToken =                 new ErrorType(name:"TokenReferencesNonToken");
-            public static readonly ErrorType TokenizedReferencesComment =               new ErrorType(name:"SubtokenReferencesToken");
-            public static readonly ErrorType AtomicInNonTokenDefinition =
-                new ErrorType(
-                    name:"AtomicInNonTokenDefinition",
-                    descriptionFormat:"Directive 'atomic' applied to the non-token definition \"{DefinitionInfoName}\" ",
-                    isWarning:true);
-            public static readonly ErrorType MindWhitespaceInNonTokenDefinition =       new ErrorType(name:"MindWhitespaceInNonTokenDefinition",        isWarning:true);
-            public static readonly ErrorType AtomicInTokenDefinition =                  new ErrorType(name:"AtomicInTokenDefinition",                   isWarning:true);
-            public static readonly ErrorType AtomicInCommentDefinition =                new ErrorType(name:"AtomicInCommentDefinition",                 isWarning:true);
-            public static readonly ErrorType MindWhitespaceInTokenizedDefinition =      new ErrorType(name:"MindWhitespaceInTokenizedDefinition",       isWarning:true);
+            public static readonly ErrorType MixedTokenizedDirectives                   = new ErrorType(name:"MixedTokenizedDirectives",                                descriptionFormat:"The definition \"{DefinitionInfoName}\" contains more than one of the 'token', 'subtoken', or 'comment' directives.");
+            public static readonly ErrorType NonTokenReferencesSubtoken                 = new ErrorType(name:"NonTokenReferencesSubtoken",                              descriptionFormat:"The non-tokenized definition '{DefinitionInfoName}' references the subtoken definition '{ReferencedDefinitionName}'.");
+            public static readonly ErrorType NonTokenReferencesComment                  = new ErrorType(name:"NonTokenReferencesComment",                               descriptionFormat:"The non-tokenized definition '{DefinitionInfoName}' references the comment definition '{ReferencedDefinitionName}'.");
+            public static readonly ErrorType TokenizedReferencesNonToken                = new ErrorType(name:"TokenizedReferencesNonToken",                             descriptionFormat:"The tokenized definition '{DefinitionInfoName}' references the non-tokenized definition '{ReferencedDefinitionName}'.");
+            public static readonly ErrorType TokenizedReferencesToken                   = new ErrorType(name:"TokenizedReferencesToken",                                descriptionFormat:"The tokenized definition '{DefinitionInfoName}' references the token definition '{ReferencedDefinitionName}'.");
+            public static readonly ErrorType TokenizedReferencesComment                 = new ErrorType(name:"TokenizedReferencesComment",                              descriptionFormat:"The tokenized definition '{DefinitionInfoName}' references the comment definition '{ReferencedDefinitionName}'.");
+            public static readonly ErrorType AtomicInNonTokenDefinition                 = new ErrorType(name:"AtomicInNonTokenDefinition",              isWarning:true, descriptionFormat:"Directive 'atomic' applied to the non-tokenized definition '{DefinitionInfoName}' is ignored.");
+            public static readonly ErrorType MindWhitespaceInNonTokenDefinition         = new ErrorType(name:"MindWhitespaceInNonTokenDefinition",      isWarning:true, descriptionFormat:"Directive 'mind whitespace' applied to the non-tokenized definition '{DefinitionInfoName}' is ignored.");
+            public static readonly ErrorType AtomicInTokenDefinition                    = new ErrorType(name:"AtomicInTokenDefinition",                 isWarning:true, descriptionFormat:"Directive 'atomic' applied to the token definition '{DefinitionInfoName}' is redundant.");
+            public static readonly ErrorType AtomicInCommentDefinition                  = new ErrorType(name:"AtomicInCommentDefinition",               isWarning:true, descriptionFormat:"Directive 'atomic' applied to the comment definition '{DefinitionInfoName}' is redundant.");
+            public static readonly ErrorType MindWhitespaceInTokenizedDefinition        = new ErrorType(name:"MindWhitespaceInTokenizedDefinition",     isWarning:true, descriptionFormat:"Directive 'mind whitespace' applied to the token, subtoken, or comment definition '{DefinitionInfoName}' is redundant.");
         }
-
     }
 }
 
