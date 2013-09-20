757f561bbd4df6cc52fcfd7b93f0aaeb5424bdf5
izrik <izrik@yahoo.com>
2013-09-20 17:04:47 -0500

    Add a test of the command line program using substituted stdin/stdout.

--- MetaphysicsIndustries.Giza.Test/CommandLineTest.cs
+++ MetaphysicsIndustries.Giza.Test/CommandLineTest.cs
@@ -1,5 +1,6 @@
 using System;
 using NUnit.Framework;
+using System.IO;
 
 namespace MetaphysicsIndustries.Giza.Test
 {
@@ -10,6 +11,37 @@ namespace MetaphysicsIndustries.Giza.Test
         public void TestCommandLine()
         {
             giza.Program.Main(new string[] { "super", "../../../giza/json.giza", "--tokenized" });
+        }
+
+        [Test]
+        public void TestCommandLine2()
+        {
+            // setup
+            string testGrammarText =
+                "expr = value ( [+-*/] value )*; " +
+                "value = ( number | varref ); " +
+                "<token> number = [\\d]+ ( '.' [\\d]* )?; " +
+                "<token> varref = [\\l]+; ";
+            StringReader newStdin = new StringReader(testGrammarText);
+            StringWriter newStdout = new StringWriter();
+            StringWriter newStderr = new StringWriter();
+            System.Console.SetIn(newStdin);
+            System.Console.SetOut(newStdout);
+            System.Console.SetError(newStderr);
+
+            // action
+            giza.Program.Main(new string[] { "super", "--tokenized", "-" });
+
+            // assertions
+            Assert.AreEqual("", newStderr.ToString());
+            Assert.AreEqual(
+                "There are 5 definitions in the grammar:\n" +
+                "  expr\n" +
+                "  value\n" +
+                "  number\n" +
+                "  varref\n" +
+                "  $implicit char class *+-/\n",
+                newStdout.ToString());
         }
     }
 }
