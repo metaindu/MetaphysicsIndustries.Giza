28989ca499b12f7edb5e88f31c8b3eebd232726c
izrik <izrik@yahoo.com>
2013-10-10 11:32:20 -0500

    Add the IndexOfNextToken field, to specify where parsing should resume after the given token.

--- Token.cs
+++ Token.cs
@@ -4,16 +4,23 @@ namespace MetaphysicsIndustries.Giza
 {
     public struct Token
     {
-        public Token(Definition definition=null, int startIndex=0, string value="")
+        public Token(Definition definition=null, int startIndex=0, string value="", int indexOfNextToken=-1)
         {
+            if (indexOfNextToken < 0)
+            {
+                indexOfNextToken = startIndex + (value ?? "").Length;
+            }
+
             Definition = definition;
             StartIndex = startIndex;
             Value = value;
+            IndexOfNextToken = indexOfNextToken;
         }
 
         public Definition Definition;
         public int StartIndex;
         public string Value;
+        public int IndexOfNextToken;
     }
 }
 
