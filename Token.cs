8dba29666f9ba82c769f38ea28f6cf2a47753413
izrik <izrik@yahoo.com>
2013-10-10 11:18:26 -0500

    Add a constructor.

--- Token.cs
+++ Token.cs
@@ -4,6 +4,13 @@ namespace MetaphysicsIndustries.Giza
 {
     public struct Token
     {
+        public Token(Definition definition=null, int startIndex=0, string value="")
+        {
+            Definition = definition;
+            StartIndex = startIndex;
+            Value = value;
+        }
+
         public Definition Definition;
         public int StartIndex;
         public string Value;
