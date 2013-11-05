966fd71d08799a03d70ad639eb1bea56880c9658
izrik <izrik@yahoo.com>
2013-11-05 10:38:32 -0600

    Add stream machinery to CharacterSource.

--- CharacterSource.cs
+++ CharacterSource.cs
@@ -41,6 +41,49 @@ namespace MetaphysicsIndustries.Giza
 
             return new InputPosition(index, line, column);
         }
+
+        public InputPosition CurrentPosition = new InputPosition(index: 0, line: 1, column: 1);
+
+        public void SetCurrentIndex(int index)
+        {
+            if (index < CurrentPosition.Index)
+            {
+                CurrentPosition = GetPosition(index);
+            }
+            else if (index > CurrentPosition.Index)
+            {
+                if (index >= Value.Length)
+                {
+                    throw new NotImplementedException();
+                }
+
+                for (; CurrentPosition.Index < index; CurrentPosition.Index++)
+                {
+                    char ch = this[CurrentPosition.Index];
+
+                    if (ch == '\n')
+                    {
+                        CurrentPosition.Line++;
+                        CurrentPosition.Column = 1;
+                    }
+                    else
+                    {
+                        CurrentPosition.Column++;
+                    }
+                }
+            }
+        }
+
+        public InputChar CurrentValue
+        {
+            get { return new InputChar(this[CurrentPosition.Index], CurrentPosition); }
+        }
+
+        public InputChar GetNextValue()
+        {
+            SetCurrentIndex(CurrentPosition.Index + 1);
+            return CurrentValue;
+        }
     }
 }
 
