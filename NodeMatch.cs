aa5588c24c06a7c1caaa7134dc174a150bbce5c7
izrik <izrik@yahoo.com>
2013-12-04 16:12:41 -0600

    Fold NodeMatchNodeMatchPreviousNextsCollection into NodeMatch, as it's not used anywhere else.

--- NodeMatch.cs
+++ NodeMatch.cs
@@ -206,93 +206,93 @@ namespace MetaphysicsIndustries.Giza
                 writer.WriteLine();
             }
         }
-    }
 
-    class NodeMatchNodeMatchPreviousNextsCollection : ICollection<NodeMatch>
-    {
-        public NodeMatchNodeMatchPreviousNextsCollection(NodeMatch container)
+        class NodeMatchNodeMatchPreviousNextsCollection : ICollection<NodeMatch>
         {
-            if (container == null) throw new ArgumentNullException("container");
-
-            _container = container;
-        }
-
-        NodeMatch _container;
-        Set<NodeMatch> _collection = new Set<NodeMatch>();
-
-        #region ICollection implementation
-
-        public void Add(NodeMatch item)
-        {
-            if (!this.Contains(item))
+            public NodeMatchNodeMatchPreviousNextsCollection(NodeMatch container)
             {
-                _collection.Add(item);
-                item.Previous = _container;
-            }
-        }
+                if (container == null) throw new ArgumentNullException("container");
 
-        public void Clear()
-        {
-            foreach (NodeMatch item in this.ToArray())
-            {
-                this.Remove(item);
-            }
-        }
-
-        public bool Contains(NodeMatch item)
-        {
-            return _collection.Contains(item);
-        }
-
-        public void CopyTo(NodeMatch[] array, int arrayIndex)
-        {
-            _collection.CopyTo(array, arrayIndex);
-        }
-
-        public bool Remove(NodeMatch item)
-        {
-            if (_collection.Remove(item))
-            {
-                item.Previous = null;
-
-                return true;
+                _container = container;
             }
 
-            return false;
-        }
+            NodeMatch _container;
+            Set<NodeMatch> _collection = new Set<NodeMatch>();
 
-        public int Count
-        {
-            get
+            #region ICollection implementation
+
+            public void Add(NodeMatch item)
             {
-                return _collection.Count;
+                if (!this.Contains(item))
+                {
+                    _collection.Add(item);
+                    item.Previous = _container;
+                }
             }
+
+            public void Clear()
+            {
+                foreach (NodeMatch item in this.ToArray())
+                {
+                    this.Remove(item);
+                }
+            }
+
+            public bool Contains(NodeMatch item)
+            {
+                return _collection.Contains(item);
+            }
+
+            public void CopyTo(NodeMatch[] array, int arrayIndex)
+            {
+                _collection.CopyTo(array, arrayIndex);
+            }
+
+            public bool Remove(NodeMatch item)
+            {
+                if (_collection.Remove(item))
+                {
+                    item.Previous = null;
+
+                    return true;
+                }
+
+                return false;
+            }
+
+            public int Count
+            {
+                get
+                {
+                    return _collection.Count;
+                }
+            }
+
+            public bool IsReadOnly
+            {
+                get { return false; }
+            }
+
+            #endregion
+
+            #region IEnumerable implementation
+
+            public IEnumerator<NodeMatch> GetEnumerator()
+            {
+                return _collection.GetEnumerator();
+            }
+
+            #endregion
+
+            #region IEnumerable implementation
+
+            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+
+            #endregion
         }
-
-        public bool IsReadOnly
-        {
-            get { return false; }
-        }
-
-        #endregion
-
-        #region IEnumerable implementation
-
-        public IEnumerator<NodeMatch> GetEnumerator()
-        {
-            return _collection.GetEnumerator();
-        }
-
-        #endregion
-
-        #region IEnumerable implementation
-
-        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        #endregion
     }
 
     public class MatchStack
