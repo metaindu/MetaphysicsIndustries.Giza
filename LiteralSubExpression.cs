33cd4e075ea5db0ada906fa569275c9c736daf56
izrik <izrik@izrik.com>
2014-08-27 10:00:40 -0500

    Render to grammar text.

--- LiteralSubExpression.cs
+++ LiteralSubExpression.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 
 namespace MetaphysicsIndustries.Giza
 {
@@ -14,6 +15,42 @@ namespace MetaphysicsIndustries.Giza
         }
 
         public string Value;
+
+        public string ToDelimitedString()
+        {
+            var chs = new List<char>();
+            chs.Add('\'');
+
+            foreach (var ch in Value)
+            {
+                if (ch == '\r' ||
+                    ch == '\n' ||
+                    ch == '\t' ||
+                    ch == '\'' ||
+                    ch == '\\')
+                {
+                    char ch2 = ch;
+                    switch (ch)
+                    {
+                        case '\r': ch2 = 'r'; break;
+                        case '\n': ch2 = 'n'; break;
+                        case '\t': ch2 = 't'; break;
+                    }
+                    chs.Add('\\');
+                    chs.Add(ch2);
+                }
+//                else if (unicode)
+//                {
+//                }
+                else
+                {
+                    chs.Add(ch);
+                }
+            }
+
+            chs.Add('\'');
+            return new string(chs.ToArray());
+        }
     }
 }
 
---
+++ MetaphysicsIndustries.Giza.Test/DefinitionRendererTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using NUnit.Framework;
+using System.Collections.Generic;
+
+namespace MetaphysicsIndustries.Giza.Test
+{
+    [TestFixture]
+    public class DefinitionRendererTest
+    {
+        [Test]
+        public void TestRenderDefinitionExprsAsGrammarText()
+        {
+            SupergrammarSpanner ss = new SupergrammarSpanner();
+            string input =
+                "expr = ( binop | subexpr );\n\n" +
+                "binop = subexpr ( [%*+-/] subexpr )+;\n\n" +
+                "subexpr = ( number | var | unop | paren );\n\n" +
+                "number = [\\d]+;\n\n" +
+                "var = [\\l]+;\n\n" +
+                "unop = [+-] subexpr;\n\n" +
+                "paren = '(' expr ')';\n\n";
+            var errors = new List<Error>();
+            var defs = ss.GetExpressions(input, errors);
+            Assert.IsEmpty(errors);
+
+            var dr = new DefinitionRenderer();
+            var result = dr.RenderDefinitionExprsAsGrammarText(defs);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void TestRenderDefinitionExprsAsGrammarTextModifiers()
+        {
+            SupergrammarSpanner ss = new SupergrammarSpanner();
+            string input =
+                "question = 'item'?;\n\n" +
+                "plus = 'item'+;\n\n" +
+                "star = 'item'*;\n\n";
+            var errors = new List<Error>();
+            var defs = ss.GetExpressions(input, errors);
+            Assert.IsEmpty(errors);
+
+            var dr = new DefinitionRenderer();
+            var result = dr.RenderDefinitionExprsAsGrammarText(defs);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void TestRenderDefinitionExprsAsGrammarTextTags()
+        {
+            SupergrammarSpanner ss = new SupergrammarSpanner();
+            string input =
+                "literal = 'item':something;\n\n" +
+                "charclass = [abc]:another;\n\n" +
+                "defref = literal:stillmore;\n\n" +
+                "tag-and-modifiers = 'item'+:thetag;\n\n";
+            var errors = new List<Error>();
+            var defs = ss.GetExpressions(input, errors);
+            Assert.IsEmpty(errors);
+
+            var dr = new DefinitionRenderer();
+            var result = dr.RenderDefinitionExprsAsGrammarText(defs);
+
+            Assert.AreEqual(input, result);
+        }
+
+        [Test]
+        public void TestRenderDefinitionExprsAsGrammarTextDirectives()
+        {
+            SupergrammarSpanner ss = new SupergrammarSpanner();
+            string input =
+                "none = 'item';\n\n" +
+                "<atomic> atomic = 'item';\n\n" +
+                "<ignore case> ignore-case = 'item';\n\n" +
+                "<mind whitespace> mind-whitespace = 'item';\n\n" +
+                "<token> token = 'item';\n\n" +
+                "<subtoken> subtoken = 'item';\n\n" +
+                "<comment> comment = 'item';\n\n" +
+                "<atomic, ignore case> multiple = 'item';\n\n";
+            var errors = new List<Error>();
+            var defs = ss.GetExpressions(input, errors);
+            Assert.IsEmpty(errors);
+
+            var dr = new DefinitionRenderer();
+            var result = dr.RenderDefinitionExprsAsGrammarText(defs);
+
+            Assert.AreEqual(input, result);
+        }
+    }
+}
+
