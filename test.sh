fb36e8297035511eabbf5f929370fc20a48a8529
izrik <izrik@izrik.com>
2014-08-26 22:29:01 -0500

    Remove unused test script.

--- test.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-mono giza/bin/Debug/giza.exe --super Supergrammar.txt -2
-mono giza/bin/Debug/giza.exe --render Supergrammar.txt Supergrammar | git diff --no-index Supergrammar.cs -
-
