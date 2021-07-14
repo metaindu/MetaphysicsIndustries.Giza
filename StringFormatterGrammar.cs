f50a2ab7c34f9bed80c149f9bc783ef1392db211
izrik <izrik@izrik.com>
2021-07-14 00:10:13 -0500

    Alphabetize.

--- StringFormatterGrammar.cs
+++ StringFormatterGrammar.cs
@@ -23,35 +23,51 @@ namespace MetaphysicsIndustries.Giza
 {
     public class StringFormatterGrammar : NGrammar
     {
-        public NDefinition def_format = new NDefinition("format");
-        public NDefinition def_text = new NDefinition("text");
         public NDefinition def_escape = new NDefinition("escape");
-        public NDefinition def_param = new NDefinition("param");
+        public NDefinition def_format = new NDefinition("format");
         public NDefinition def_name = new NDefinition("name");
+        public NDefinition def_param = new NDefinition("param");
+        public NDefinition def_text = new NDefinition("text");
 
-        public DefRefNode node_format_0_text;
-        public DefRefNode node_format_1_escape;
-        public DefRefNode node_format_2_param;
-        public CharNode node_text_0__005E__007B__007D_;
         public CharNode node_escape_0__007B__007B_;
         public CharNode node_escape_1__007B__007B_;
         public CharNode node_escape_2__007D__007D_;
         public CharNode node_escape_3__007D__007D_;
+        public DefRefNode node_format_0_text;
+        public DefRefNode node_format_1_escape;
+        public DefRefNode node_format_2_param;
+        public CharNode node_name_0__005C_l_005F_;
+        public CharNode node_name_1__005C_l_005C_d;
         public CharNode node_param_0__007B_;
         public CharNode node_param_1__005C_s;
         public DefRefNode node_param_2_name;
         public CharNode node_param_3__005C_s;
         public CharNode node_param_4__007D_;
-        public CharNode node_name_0__005C_l_005F_;
-        public CharNode node_name_1__005C_l_005C_d;
+        public CharNode node_text_0__005E__007B__007D_;
 
         public StringFormatterGrammar()
         {
-            Definitions.Add(def_format);
-            Definitions.Add(def_text);
             Definitions.Add(def_escape);
-            Definitions.Add(def_param);
+            Definitions.Add(def_format);
             Definitions.Add(def_name);
+            Definitions.Add(def_param);
+            Definitions.Add(def_text);
+
+            def_escape.Directives.Add(DefinitionDirective.MindWhitespace);
+            node_escape_0__007B__007B_ = new CharNode(CharClass.FromUndelimitedCharClassText("{"), "{{");
+            node_escape_1__007B__007B_ = new CharNode(CharClass.FromUndelimitedCharClassText("{"), "{{");
+            node_escape_2__007D__007D_ = new CharNode(CharClass.FromUndelimitedCharClassText("}"), "}}");
+            node_escape_3__007D__007D_ = new CharNode(CharClass.FromUndelimitedCharClassText("}"), "}}");
+            def_escape.Nodes.Add(node_escape_0__007B__007B_);
+            def_escape.Nodes.Add(node_escape_1__007B__007B_);
+            def_escape.Nodes.Add(node_escape_2__007D__007D_);
+            def_escape.Nodes.Add(node_escape_3__007D__007D_);
+            def_escape.StartNodes.Add(node_escape_0__007B__007B_);
+            def_escape.StartNodes.Add(node_escape_2__007D__007D_);
+            def_escape.EndNodes.Add(node_escape_1__007B__007B_);
+            def_escape.EndNodes.Add(node_escape_3__007D__007D_);
+            node_escape_0__007B__007B_.NextNodes.Add(node_escape_1__007B__007B_);
+            node_escape_2__007D__007D_.NextNodes.Add(node_escape_3__007D__007D_);
 
             def_format.Directives.Add(DefinitionDirective.MindWhitespace);
             node_format_0_text = new DefRefNode(def_text, "text");
@@ -76,29 +92,16 @@ namespace MetaphysicsIndustries.Giza
             node_format_2_param.NextNodes.Add(node_format_1_escape);
             node_format_2_param.NextNodes.Add(node_format_2_param);
 
-            def_text.Directives.Add(DefinitionDirective.Atomic);
-            def_text.Directives.Add(DefinitionDirective.MindWhitespace);
-            node_text_0__005E__007B__007D_ = new CharNode(CharClass.FromUndelimitedCharClassText("^{}"), "^{}");
-            def_text.Nodes.Add(node_text_0__005E__007B__007D_);
-            def_text.StartNodes.Add(node_text_0__005E__007B__007D_);
-            def_text.EndNodes.Add(node_text_0__005E__007B__007D_);
-            node_text_0__005E__007B__007D_.NextNodes.Add(node_text_0__005E__007B__007D_);
-
-            def_escape.Directives.Add(DefinitionDirective.MindWhitespace);
-            node_escape_0__007B__007B_ = new CharNode(CharClass.FromUndelimitedCharClassText("{"), "{{");
-            node_escape_1__007B__007B_ = new CharNode(CharClass.FromUndelimitedCharClassText("{"), "{{");
-            node_escape_2__007D__007D_ = new CharNode(CharClass.FromUndelimitedCharClassText("}"), "}}");
-            node_escape_3__007D__007D_ = new CharNode(CharClass.FromUndelimitedCharClassText("}"), "}}");
-            def_escape.Nodes.Add(node_escape_0__007B__007B_);
-            def_escape.Nodes.Add(node_escape_1__007B__007B_);
-            def_escape.Nodes.Add(node_escape_2__007D__007D_);
-            def_escape.Nodes.Add(node_escape_3__007D__007D_);
-            def_escape.StartNodes.Add(node_escape_0__007B__007B_);
-            def_escape.StartNodes.Add(node_escape_2__007D__007D_);
-            def_escape.EndNodes.Add(node_escape_1__007B__007B_);
-            def_escape.EndNodes.Add(node_escape_3__007D__007D_);
-            node_escape_0__007B__007B_.NextNodes.Add(node_escape_1__007B__007B_);
-            node_escape_2__007D__007D_.NextNodes.Add(node_escape_3__007D__007D_);
+            def_name.Directives.Add(DefinitionDirective.MindWhitespace);
+            node_name_0__005C_l_005F_ = new CharNode(CharClass.FromUndelimitedCharClassText("\\l_"), "\\l_");
+            node_name_1__005C_l_005C_d = new CharNode(CharClass.FromUndelimitedCharClassText("\\l\\d"), "\\l\\d");
+            def_name.Nodes.Add(node_name_0__005C_l_005F_);
+            def_name.Nodes.Add(node_name_1__005C_l_005C_d);
+            def_name.StartNodes.Add(node_name_0__005C_l_005F_);
+            def_name.EndNodes.Add(node_name_1__005C_l_005C_d);
+            def_name.EndNodes.Add(node_name_0__005C_l_005F_);
+            node_name_0__005C_l_005F_.NextNodes.Add(node_name_1__005C_l_005C_d);
+            node_name_1__005C_l_005C_d.NextNodes.Add(node_name_1__005C_l_005C_d);
 
             def_param.Directives.Add(DefinitionDirective.MindWhitespace);
             node_param_0__007B_ = new CharNode(CharClass.FromUndelimitedCharClassText("{"), "{");
@@ -122,16 +125,13 @@ namespace MetaphysicsIndustries.Giza
             node_param_3__005C_s.NextNodes.Add(node_param_3__005C_s);
             node_param_3__005C_s.NextNodes.Add(node_param_4__007D_);
 
-            def_name.Directives.Add(DefinitionDirective.MindWhitespace);
-            node_name_0__005C_l_005F_ = new CharNode(CharClass.FromUndelimitedCharClassText("\\l_"), "\\l_");
-            node_name_1__005C_l_005C_d = new CharNode(CharClass.FromUndelimitedCharClassText("\\l\\d"), "\\l\\d");
-            def_name.Nodes.Add(node_name_0__005C_l_005F_);
-            def_name.Nodes.Add(node_name_1__005C_l_005C_d);
-            def_name.StartNodes.Add(node_name_0__005C_l_005F_);
-            def_name.EndNodes.Add(node_name_1__005C_l_005C_d);
-            def_name.EndNodes.Add(node_name_0__005C_l_005F_);
-            node_name_0__005C_l_005F_.NextNodes.Add(node_name_1__005C_l_005C_d);
-            node_name_1__005C_l_005C_d.NextNodes.Add(node_name_1__005C_l_005C_d);
+            def_text.Directives.Add(DefinitionDirective.Atomic);
+            def_text.Directives.Add(DefinitionDirective.MindWhitespace);
+            node_text_0__005E__007B__007D_ = new CharNode(CharClass.FromUndelimitedCharClassText("^{}"), "^{}");
+            def_text.Nodes.Add(node_text_0__005E__007B__007D_);
+            def_text.StartNodes.Add(node_text_0__005E__007B__007D_);
+            def_text.EndNodes.Add(node_text_0__005E__007B__007D_);
+            node_text_0__005E__007B__007D_.NextNodes.Add(node_text_0__005E__007B__007D_);
 
         }
     }
--- render_grammars.sh
+++ render_grammars.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 giza render Supergrammar Supergrammar.txt > Supergrammar2.cs
+giza render StringFormatterGrammar StringFormatterGrammar.giza > StringFormatterGrammar2.cs
