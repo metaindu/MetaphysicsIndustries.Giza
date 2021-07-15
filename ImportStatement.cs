6515febe81575ba65e203240f004aa98db78f667
izrik <izrik@izrik.com>
2021-07-14 22:43:23 -0500

    todo

--- ImportStatement.cs
+++ ImportStatement.cs
@@ -2,6 +2,7 @@ namespace MetaphysicsIndustries.Giza
 {
     public struct ImportStatement
     {
+        // TODO: module imports
         public bool IsModuleImport;
         public string ModuleOrFile;
         public ImportRef[] ImportRefs;
