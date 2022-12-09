c1956a525ab3db322503f7d35131a15c232075e9
izrik <izrik@izrik.com>
2022-12-09 17:00:58 -0600

    Update copyright year.

--- giza/Properties/AssemblyInfo.cs
+++ giza/Properties/AssemblyInfo.cs
@@ -1,6 +1,6 @@
 ﻿
 // MetaphysicsIndustries.Giza - A Parsing System
-// Copyright (C) 2008-2021 Metaphysics Industries, Inc.
+// Copyright (C) 2008-2022 Metaphysics Industries, Inc.
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -26,7 +26,8 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Metaphysics Industries, Inc.")]
 [assembly: AssemblyProduct("giza")]
-[assembly: AssemblyCopyright("Copyright © Metaphysics Industries, Richard Sartor 2021")]
+[assembly: AssemblyCopyright(
+    "Copyright © Metaphysics Industries, Richard Sartor 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
