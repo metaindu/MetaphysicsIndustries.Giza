33d7cd2f934b416011df87a207df26c99f3ce5dc
izrik <izrik@izrik.com>
2022-12-09 14:15:00 -0600

    Fix assembly version numbers.

--- giza/Properties/AssemblyInfo.cs
+++ giza/Properties/AssemblyInfo.cs
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.10.0.0")]
-[assembly: AssemblyFileVersion("0.10.0.0")]
+[assembly: AssemblyVersion("0.3.0.0")]
+[assembly: AssemblyFileVersion("0.3.0.0")]
